0222db1252842ed395468f84838ff7c43100f306
MagiMetal <justinbauer.web@gmail.com>
2022-06-09 18:14:18 -0400

    updates, design changes, support/tip button, responsive changes

--- scripts.js
+++ scripts.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 if (window.location.protocol !== 'https:') {
-  // location.href = location.href.replace('http://', 'https://');
+  location.href = location.href.replace('http://', 'https://');
 }
 
 const toggleVisibility = () => {
